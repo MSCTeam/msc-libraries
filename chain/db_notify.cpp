3be2dc05b52439986e5958b158086a474908682f
MSCTeam <MSCTeam@protonmail.com>
2021-04-01 21:30:08 +0800

    update clear_expired_transactions

--- chain/db_notify.cpp
+++ chain/db_notify.cpp
@@ -295,6 +295,17 @@ void graphene::chain::transaction_get_impacted_accounts( const transaction& tx, 
     operation_get_impacted_accounts( op, result );
 }
 
+void database::clear_expired_transactions()
+{ try {
+   //Look for expired transactions in the deduplication list, and remove them.
+   //Transactions must have expired by at least two forking windows in order to be removed.
+   auto& transaction_idx = static_cast<transaction_index&>(get_mutable_index(implementation_ids,
+                                                                             impl_transaction_history_object_type));
+   const auto& dedupe_index = transaction_idx.indices().get<by_expiration>();
+   while( (!dedupe_index.empty()) && (head_block_time() > dedupe_index.begin()->trx.expiration) )
+      transaction_idx.remove(*dedupe_index.begin());
+} FC_CAPTURE_AND_RETHROW() }
+
 void get_relevant_accounts( const object* obj, flat_set<account_id_type>& accounts )
 {
    if( obj->id.space() == protocol_ids )
